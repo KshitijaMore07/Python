79daa0a0a3d5147eb8fc47d614827f4de1628d73
Kshitija More <ksmore346@gmail.com>
2025-11-08 22:14:59 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,11 @@
+veggies=["potato","brinjal","ladyfinger","cucumber"]
+
+for val in veggies:
+    print(val)
+
+
+
+tup=(1,2,3,4,2,8,9)
+
+for num in tup:
+    print(num)
---
+++ for1.py
@@ -0,0 +1,20 @@
+str="fergusson college"
+
+for char in str:
+    print(char)
+else:
+    print("end")
+
+
+
+
+
+str="fergusson college"
+
+for char in str:
+    if(char=='o'):
+        print("o found")
+        break
+
+
+  
