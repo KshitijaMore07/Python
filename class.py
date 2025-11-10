5fcbc32349fbace1d2cf76627f530de8dcef68f2
Kshitija More <ksmore346@gmail.com>
2025-11-10 21:57:00 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,4 @@
+class Student:
+    name="karan kumar"
+s1=Student()
+print(s1.name)
---
+++ substring.py
@@ -0,0 +1,7 @@
+text=input("Enter a string: ")
+old_sub=input("Enter the word to replace: ")
+new_sub=input("Enter the new word: ")
+
+new_text=text.replace(old_sub, new_sub)
+
+print("Updated string:",new_text)
